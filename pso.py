c53efd61df5b5812029d475ae489e86d58cb60dd
taotao <zouzhitao_cq@163.com>
2017-10-06 15:00:59 +0800

    修改参数

--- pso.py
+++ pso.py
@@ -6,7 +6,7 @@
 """
 import numpy as np
 from numpy.linalg import svd, norm
-from utils import State
+from utils import State, bound
 
 # 高斯退火常数
 ANNEAL_CONST = 0.2
@@ -18,6 +18,7 @@ class Particle(object):
         self.pbest = best
         l, r = best.get_rec()
         self.x = State((0, 0), (r[0]-l[0], r[1]-l[1]))
+        self.v = np.random.randn(2)
         self.fit = 0
 
 
@@ -26,20 +27,23 @@ class Object(object):
         self.A = objmat
         self.U, self.sigma, self.Vh = svd(objmat, full_matrices=False)
         self.mul_mat = self.U.dot(self.U.T)
+        self.tmp = self.mul_mat.dot(objmat)
     def update_space_mat(self,e):
         self.A = np.hstack((self.A, e))
         self.U, self.sigma, self.vh = svd(self.A, full_matrices=False)
         self.mul_mat = self.U.dot(self.U.T)
+        self.tmp = self.mul_mat.dot(e)
 
 
 class Tracker(object):
     # max num of particles
-    NUM_PARTICLE = 25
+    NUM_PARTICLE = 50
 
     # PSO const param
-    R1, R2, R3 = np.abs(np.random.randn(3))
+    #R1, R2, R3 = np.abs(np.random.randn(3))
+    R1, R2, R3 = 1., 1.49, 1.49
     # gauss transition matrix
-    GAUSS = np.eye(2,2)*2
+    GAUSS = np.eye(2, 2)*2
     # anneal cov matrix
     ANNEAL_MAT = np.eye(2)*2
     # max iter num
@@ -63,11 +67,15 @@ class Tracker(object):
     def fitness_value_unocc(self, s, img):
         # 非重叠实现
         left, right = s.get_rec()
+        if not bound(img.shape,left, right):
+            return -1e5
+
         o = img[left[0]:right[0], left[1]:right[1]]
         o = o.reshape((o.size, -1))
-        RE = norm(o - self.obj.mul_mat.dot(o), 2)
+        RE = norm(o - self.obj.tmp, 2)
+        #print('RE', RE)
         return np.exp(-RE ** 2)
-
+        #return -RE
     def update_particle_unocc(self, idx, iters):
         """
         更新粒子状态,非重叠实现
@@ -79,9 +87,12 @@ class Tracker(object):
         pbest = self.par[idx].pbest.to_addable_vec()
         gbest = self.gbest.to_addable_vec()
         x_mat = self.par[idx].x.to_addable_vec()
-        v_mat = self.R1 * (pbest - x_mat) + self.R2 * (gbest - x_mat) + self.anneal_const(iters)
-        x_mat = x_mat + v_mat
+
+        self.par[idx].v = self.R1 * (pbest - x_mat) + self.R2 * (gbest - x_mat) + self.anneal_const(iters)
+
+        x_mat = x_mat + self.par[idx].v
         self.par[idx].x.update_left(x_mat)
+
 
     def tracking_unocc(self, img):
         """
@@ -100,23 +111,23 @@ class Tracker(object):
                 try:
                     fit = self.fitness_value_unocc(self.par[i].x, img)
                 except :
-                    print(i)
-                    fit = self.fitness_value_unocc(self.par[i].x, img)
+                    print('---------------errr------------', i)
                     print(fit)
                     print(self.par[i].x)
                     print(img.shape)
                     print(self.par[i].x.get_rec())
+                    #Tracker.R1,Tracker.R2,Tracker.R3 = np.abs(np.random.randn(3))
+                    #fit = self.fitness_value_unocc(self.par[i].x, img)
 
                 if fit > self.par[i].fit:
-                    print('fit', fit)
                     self.par[i].pbest = self.par[i].x
                     self.par[i].fit = fit
                     if fit > gfit:
                         self.gbest = self.par[i].pbest
                         gfit = fit
-                        print(self.gbest.to_addable_vec(), gfit)
-            if gfit > TH:
-                break
+
+            print(_, gfit)
+
 
         left, right = self.gbest.get_rec()
         e = img[left[0]:right[0], left[1]:right[1]]
